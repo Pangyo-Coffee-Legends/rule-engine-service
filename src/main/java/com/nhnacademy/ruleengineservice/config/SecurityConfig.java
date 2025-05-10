97fcad6d12e3666d09b58e3bb1107ac8e4ebb418
oculusK <kay5990@naver.com>
2025-05-10 22:03:01 +0900

    feature API 인증 기능 disable

--- src/main/java/com/nhnacademy/ruleengineservice/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/ruleengineservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nhnacademy.ruleengineservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,14 +12,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable) // API 서버는 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/comfort/**",
                                 "/api/v1/members/**"
                         ).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable); // 브라우저나 클라이언트 인증 요청 비활성화
         return http.build();
     }
 }
