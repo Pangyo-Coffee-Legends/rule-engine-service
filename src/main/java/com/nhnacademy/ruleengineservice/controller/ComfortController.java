e9a2145bfd01d45e6f434ce8c5c406ba2da82d31
oculusK <kay5990@naver.com>
2025-05-12 14:33:54 +0900

    feature 기능 수정

--- src/main/java/com/nhnacademy/ruleengineservice/controller/ComfortController.java
+++ src/main/java/com/nhnacademy/ruleengineservice/controller/ComfortController.java
@@ -1,14 +1,15 @@
 package com.nhnacademy.ruleengineservice.controller;
 
 import com.nhnacademy.ruleengineservice.dto.comfort.ComfortInfoDTO;
+import com.nhnacademy.ruleengineservice.dto.engine.RuleEvaluationResult;
 import com.nhnacademy.ruleengineservice.schedule.ComfortInfoBuffer;
+import com.nhnacademy.ruleengineservice.service.schedule.ComfortResultService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -16,11 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ComfortController {
     private final ComfortInfoBuffer buffer;
+    private final ComfortResultService comfortResultService;
 
     @PostMapping
     public ResponseEntity<Void> receiveComfortInfo(@RequestBody ComfortInfoDTO comfortInfo) {
         log.debug("받은 정보: {}", comfortInfo);
         buffer.add(comfortInfo);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/scheduled-result")
+    public ResponseEntity<List<RuleEvaluationResult>> getScheduledResult() {
+        log.debug("scheduled-result success");
+        return ResponseEntity.ok(comfortResultService.getLatestResults());
     }
 }
--- src/test/java/com/nhnacademy/ruleengineservice/controller/ComfortControllerTest.java
+++ src/test/java/com/nhnacademy/ruleengineservice/controller/ComfortControllerTest.java
@@ -2,9 +2,13 @@ package com.nhnacademy.ruleengineservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.ruleengineservice.dto.comfort.ComfortInfoDTO;
+import com.nhnacademy.ruleengineservice.dto.engine.RuleEvaluationResult;
 import com.nhnacademy.ruleengineservice.schedule.ComfortInfoBuffer;
+import com.nhnacademy.ruleengineservice.service.schedule.ComfortResultService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,10 +17,14 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ComfortController.class)
@@ -29,13 +37,18 @@ class ComfortControllerTest {
     @MockitoBean
     private ComfortInfoBuffer buffer;
 
+    @MockitoBean
+    ComfortResultService comfortResultService;
+
     @Autowired
     ObjectMapper objectMapper;
 
-    @Test
-    @DisplayName("AI 정상 동작")
-    void testReceiveComfortInfo() throws Exception {
-        ComfortInfoDTO comfortInfo = new ComfortInfoDTO(
+    private ComfortInfoDTO testComfortInfo;
+    private List<RuleEvaluationResult> testResults;
+
+    @BeforeEach
+    void setUp() {
+        testComfortInfo = new ComfortInfoDTO(
                 "A",
                 LocalDateTime.now(),
                 30.0,
@@ -45,12 +58,38 @@ class ComfortControllerTest {
                 "CO2 주의"
         );
 
+        testResults = Collections.singletonList(
+                new RuleEvaluationResult(
+                        1L,
+                        "rule-001",
+                        true
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("AI 정상 동작")
+    void testReceiveComfortInfo() throws Exception {
         mockMvc.perform(post("/api/v1/comfort")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(comfortInfo)))
+                        .content(objectMapper.writeValueAsString(testComfortInfo)))
                 .andExpect(status().isOk());
 
         // buffer.add 가 호출됐는지 검증
         verify(buffer).add(any(ComfortInfoDTO.class));
+    }
+
+    @Test
+    @DisplayName("스케줄 결과 조회")
+    void testGetScheduledResult() throws Exception {
+        // Given
+        Mockito.when(comfortResultService.getLatestResults())
+                .thenReturn(testResults);
+
+        // When & Then
+        mockMvc.perform(get("/api/v1/comfort/scheduled-result"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].ruleName").value("rule-001"))
+                .andExpect(jsonPath("$[0].success").value(true));
     }
 }
