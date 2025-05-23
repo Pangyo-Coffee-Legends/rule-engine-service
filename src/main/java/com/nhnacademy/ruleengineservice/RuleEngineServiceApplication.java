309b30d35c76faae48c2f8084c9df76a284e4534
oculusK <kay5990@naver.com>
2025-05-23 17:14:39 +0900

    feature Feign 클라이언트 관련 추가

--- src/main/java/com/nhnacademy/ruleengineservice/RuleEngineServiceApplication.java
+++ src/main/java/com/nhnacademy/ruleengineservice/RuleEngineServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 public class RuleEngineServiceApplication {
 
